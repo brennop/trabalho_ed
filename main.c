88936ac0bfac7bffb7232473857137e879e8f46b
brennop <brennopcordeiro@gmail.com>
2020-01-27 11:51:37 -0300

    :sparkles: Abstrai lista encadeada

--- main.c
+++ main.c
@@ -11,7 +11,6 @@ typedef struct _aluno {
   float faltas;
   float nota;
   char mencao[4];
-  struct _aluno *prox;
 } aluno;
 
 // TAD de disciplinas
@@ -26,8 +25,11 @@ typedef struct _item {
 } item;
 
 typedef struct _lista {
-  aluno *inicio;
+  item *inicio;
+  item *final;
 } lista;
+
+void inserirFinal(lista *LISTA, void *tad);
 
 lista *lerLista();
 void salvarLista(lista *LISTA);
@@ -73,21 +75,34 @@ int main() {
   return 0;
 }
 
+void inserirFinal(lista *LISTA, void *tad) {
+  item *i = (item *)malloc(sizeof(item));
+  i->atual = tad;
+
+  if (LISTA->inicio == NULL) { // Lista vazia
+    LISTA->inicio = i;
+  } else {
+    LISTA->final->prox = i;
+  }
+  LISTA->final = i;
+}
+
 // Lê do arquivo como binário
 // deve ser mudada posteriormente
 // para ler do arquivo em texto
 lista *lerLista() {
   lista *l = (lista *)malloc(sizeof(lista));
   FILE *arquivo = fopen("alunos.txt", "rb");
-  aluno *tmp = (aluno *)malloc(sizeof(aluno));
-  while (fread(tmp, sizeof(aluno), 1, arquivo)) {
-    aluno *a = (aluno *)malloc(sizeof(aluno));
-    *a = *tmp;
-    a->prox = l->inicio;
-    l->inicio = a;
+  if (arquivo) {
+    aluno *tmp = (aluno *)malloc(sizeof(aluno));
+    while (fread(tmp, sizeof(aluno), 1, arquivo)) {
+      aluno *a = (aluno *)malloc(sizeof(aluno));
+      *a = *tmp;
+      inserirFinal(l, a);
+    }
+    free(tmp);
+    fclose(arquivo);
   }
-  free(tmp);
-  fclose(arquivo);
   return l;
 }
 
@@ -96,18 +111,19 @@ lista *lerLista() {
 // para salvar como arquivo de texto
 void salvarLista(lista *LISTA) {
   FILE *arquivo = fopen("alunos.txt", "wb");
-  aluno *atual = LISTA->inicio;
+  item *atual = LISTA->inicio;
   while (atual) {
-    fwrite(atual, sizeof(aluno), 1, arquivo);
+    fwrite(atual->atual, sizeof(aluno), 1, arquivo);
     atual = atual->prox;
   }
   fclose(arquivo);
 }
 
 void mostrarLista(lista *LISTA) {
-  aluno *atual = LISTA->inicio;
+  item *atual = LISTA->inicio;
   while (atual) {
-    printf("%s %d %s\n", atual->nome, atual->matricula, atual->curso);
+    aluno *a = atual->atual;
+    printf("%s %d %s\n", a->nome, a->matricula, a->curso);
     atual = atual->prox;
   }
 }
@@ -123,6 +139,5 @@ void adicionarAluno(lista *LISTA) {
   printf("Curso: ");
   scanf("%s", novo->curso);
 
-  novo->prox = LISTA->inicio;
-  LISTA->inicio = novo;
+  inserirFinal(LISTA, novo);
 }
